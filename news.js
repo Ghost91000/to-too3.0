1ff951da467ebed0c80d57ac77c965536bca195f
Vlad <vlad.velichko.90@gmail.com>
2019-02-05 19:03:48 +0200

    Update news.js

--- news.js
+++ news.js
@@ -17,6 +17,25 @@ function whatNew(){
 function close() {
   var b = document.getElementById("buton");
   b.innerHTML = "Close";
-  b.onclick = false;
-  b.href = "";
+  b.onclick = function () {buttonClose();};
+}
+function buttonClose () {
+  var news = document.getElementById("news");
+  news.style.opacity = "1";
+  document.getElementById("tNews").innerHTML = "News:";
+
+  var list = document.getElementById("whatNew");
+  var p = 90;
+  var time = setInterval(function (){
+    if (p == 20){clearInterval(time); again();}
+    else{
+      list.style.height = p+"%";
+      p -= 1;
+    }
+  },5);
+}
+function again () {
+  var b = document.getElementById("buton");
+  b.innerHTML = "See more";
+  b.onclick = function () {whatNew();};
 }
