dd19c920609e5abbe98e9008400018fa90ffc994
lizonn <lizonn@users.noreply.github.com>
2019-02-09 17:11:02 +0300

    it doesn't work(

---
+++ myPage.js
@@ -0,0 +1,6 @@
+var butPhoto = document.getElementById('mainPhoto');
+var modalwindow1 = document.getElementsByClassName('modalPhotos');
+
+butPhoto.onclick = function () {
+  modalwindow1.style.display="block";
+}
