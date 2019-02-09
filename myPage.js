5a4c31f4da2a8512884456c9e36ae3c5481bc171
lizonn <lizonn@users.noreply.github.com>
2019-02-10 00:38:23 +0300

    working window
    but not a slider

--- myPage.js
+++ myPage.js
@@ -1,6 +1,29 @@
-var butPhoto = document.getElementById('mainPhoto');
-var modalwindow1 = document.getElementsByClassName('modalPhotos');
 
-butPhoto.onclick = function () {
-  modalwindow1.style.display="block";
+var modalwindow = document.getElementById ("modalPhotos");
+ function openn () {
+     var modalwindow = document.getElementById ("modalPhotos");
+  modalwindow.style.display='block';
+}
+console.log(document.getElementById('modalPhotos'));
+
+function closess() {
+   var modalwindow = document.getElementById ("modalPhotos");
+modalwindow.style.display="none";
+
+}
+
+
+function slider () {
+  var masPhoto =[
+    "examplePhoto1.jpg",
+    "examplePhoto2.jpg",
+    "bestTriangle.jng"
+  ]
+  var num=0;
+  var sliderr = document.getElementById("sliderr");
+  num++;
+  if (num>=masPhoto.lenght) {
+    num=0;
+  }
+  sliderr.src = masPhoto[num];
 }
